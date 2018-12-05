d1759383b74a9371a0facad0401cef1e54eb5595
Robby <ta7382@gmail.com>
2018-12-05 16:20:21 +0800

    Add textarea auto height

--- src/components/TranslateBox.js
+++ src/components/TranslateBox.js
@@ -5,12 +5,22 @@ class TranslateBox extends Component {
    constructor(props) {
     super(props);
     this.state = {
-      text: '',
+      text: 'Hello Pokemon',
+      row: {
+        height: '200px',
+      },
     };
   }
 
-  targethandler(e){
-    this.setState({text:e.target.value});
+  targetHandler(e){
+    const length = e.target.value.split('\n').length - 4;
+    const height = length > 0 ? 40 * length + 200 : 200;
+    this.setState({
+      text:e.target.value,
+      row: {
+        height: `${height}px`,
+      },
+    });
   }
 
   render() {
@@ -20,6 +30,7 @@ class TranslateBox extends Component {
           <div className="col" datatype="source">
             <div className="translate-title">English</div>
             <textarea
+              style={this.state.row}
               id="textSource"
               className="translate-text"
               rows="1" 
@@ -27,13 +38,15 @@ class TranslateBox extends Component {
               autoCapitalize="off" 
               autoComplete="off" 
               autoCorrect="off"
-              onChange={(e)=>this.targethandler(e)}
+              onChange={(e)=>this.targetHandler(e)}
+              value={this.state.text}
             >
             </textarea>
           </div>
           <div className="col" datatype="target">
             <div className="translate-title">Unown</div>
             <textarea
+              style={this.state.row}
               id="textTarget"
               className="translate-text"
               rows="1" 
