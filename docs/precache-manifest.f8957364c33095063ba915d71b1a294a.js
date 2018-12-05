a1874ff080a15549dffc17f1154fe2314b660899
Robby <ta7382@gmail.com>
2018-12-05 14:24:02 +0800

    Add env to set PUBLIC_URL

---
+++ docs/precache-manifest.f8957364c33095063ba915d71b1a294a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6120639772c6c60a2fad6742051c6feb",
+    "url": "./static/media/Unown.61206397.ttf"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ac28e2bf2443c03382b0",
+    "url": "./static/js/main.ac28e2bf.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "./static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "ac28e2bf2443c03382b0",
+    "url": "./static/css/main.61e68014.chunk.css"
+  },
+  {
+    "revision": "6d5b96e1478e5cf92a7cf9b027a2d1a0",
+    "url": "./index.html"
+  }
+];
