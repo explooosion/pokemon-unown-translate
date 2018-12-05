5a442b8aa5a236aa9a738664de91c1bcac24b173
Robby <ta7382@gmail.com>
2018-12-05 14:19:57 +0800

    Update website

---
+++ docs/precache-manifest.c48201b3761dc229653821e047f2ff7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6120639772c6c60a2fad6742051c6feb",
+    "url": "/static/media/Unown.61206397.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "feb6990b7f2d171c7c48",
+    "url": "/static/js/main.feb6990b.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "feb6990b7f2d171c7c48",
+    "url": "/static/css/main.66d67ca6.chunk.css"
+  },
+  {
+    "revision": "7498fd43b4ba617b433de80802af318c",
+    "url": "/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/explooosion/pokemon-unown-translate/precache-manifest.5cffd9c38e05b00a1e2745b68ac246e4.js"
+  "/precache-manifest.c48201b3761dc229653821e047f2ff7b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/explooosion/pokemon-unown-translate/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
